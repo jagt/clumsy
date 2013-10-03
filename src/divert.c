fb08c293df435cc36c117ee5fb80da60f741a526
jagt <jagt@live.com>
2013-10-03 23:25:38 +0800

    implement debug log packet

--- src/divert.c
+++ src/divert.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <memory.h>
 #include <assert.h>
+#include <winsock2.h>
 #include "divert.h"
 #include "common.h"
 #define CLUMPSY_DIVERT_PRIORITY 0
@@ -12,6 +13,49 @@ static volatile short stopLooping;
 static DWORD divertReadLoop(LPVOID arg);
 static HANDLE loopThread;
 
+#ifdef _DEBUG
+PDIVERT_IPHDR ip_header;
+PDIVERT_IPV6HDR ipv6_header;
+PDIVERT_TCPHDR tcp_header;
+PDIVERT_UDPHDR udp_header;
+UINT payload_len;
+//char pacBuf[MSG_BUFSIZE];
+void dumpPacket(char *buf, int len, PDIVERT_ADDRESS paddr) {
+    UINT16 srcPort = 0, dstPort = 0;
+
+    DivertHelperParsePacket(buf, len, &ip_header, &ipv6_header,
+        NULL, NULL, &tcp_header, &udp_header,
+        NULL, &payload_len);
+    // need to cast byte order on port numbers
+    if (tcp_header != NULL) {
+        srcPort = ntohs(tcp_header->SrcPort);
+        dstPort = ntohs(tcp_header->DstPort);
+    } else if (udp_header != NULL) {
+        srcPort = ntohs(udp_header->SrcPort);
+        dstPort = ntohs(udp_header->DstPort);
+    }
+
+    if (ip_header != NULL) {
+        UINT8 *src_addr = (UINT8*)&ip_header->SrcAddr;
+        UINT8 *dst_addr = (UINT8*)&ip_header->DstAddr;
+        LOG("%s: %u.%u.%u.%u:%d->%u.%u.%u.%u:%d",
+            paddr->Direction == DIVERT_DIRECTION_OUTBOUND ? "OUT " : "IN  ",
+            src_addr[0], src_addr[1], src_addr[2], src_addr[3], srcPort,
+            dst_addr[0], dst_addr[1], dst_addr[2], dst_addr[3], dstPort);
+    } else if (ipv6_header != NULL) {
+        UINT16 *src_addr6 = (UINT16*)&ipv6_header->SrcAddr;
+        UINT16 *dst_addr6 = (UINT16*)&ipv6_header->DstAddr;
+        LOG("%s: %x:%x:%x:%x:%x:%x:%x:%x:%d->%x:%x:%x:%x:%x:%x:%x:%x:%d",
+            paddr->Direction == DIVERT_DIRECTION_OUTBOUND ? "OUT " : "IN  ",
+            src_addr6[0], src_addr6[1], src_addr6[2], src_addr6[3],
+            src_addr6[4], src_addr6[5], src_addr6[6], src_addr6[7], srcPort,
+            dst_addr6[0], dst_addr6[1], dst_addr6[2], dst_addr6[3],
+            dst_addr6[4], dst_addr6[5], dst_addr6[6], dst_addr6[7], dstPort);
+    }
+}
+#else
+#define dumpPacket(x, y, z)
+#endif
 
 int divertStart(const char * filter, char buf[]) {
     int ix;
@@ -74,6 +118,7 @@ static DWORD divertReadLoop(LPVOID arg) {
             LOG("Interal Error: DivertRecv truncated recv packet."); 
         }
 
+        dumpPacket(packetBuf, readLen, &addrBuf);  
         // create node and put it into the list
         pnode = createNode(packetBuf, readLen, &addrBuf);
         appendNode(pnode);
